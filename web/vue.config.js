17a8f460497091375fc2e4b9a1ef8af891510169
RJ <rakhimov.5556@gmail.com>
2019-06-28 17:27:56 +0500

    feat: seo optimization

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,4 +1,15 @@
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
+
 module.exports = {
   publicPath: "./",
-  assetsDir: "assets"
+  assetsDir: "assets",
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, "dist"),
+        routes: ["/"]
+      })
+    ]
+  }
 };
