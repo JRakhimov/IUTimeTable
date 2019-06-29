9da09eae9acac8a17f88709c7ce4fdc6cd28f98a
RJ <rakhimov.5556@gmail.com>
2019-06-29 21:06:46 +0500

    feat: firebase push notifications

--- web/src/main.js
+++ web/src/main.js
@@ -1,14 +1,46 @@
 import Vue from "vue";
+import Vuetify from "vuetify";
+import * as firebase from "firebase/app";
+
 import App from "./App.vue";
 import store from "./store";
 import { router } from "./router";
+
 import "./registerServiceWorker";
+import "firebase/firebase-messaging";
+import "vuetify/dist/vuetify.min.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.prototype.HOST_URL = process.env.VUE_APP_HOST_URL;
 
-import Vuetify from "vuetify";
-import "material-design-icons-iconfont/dist/material-design-icons.css";
-import "vuetify/dist/vuetify.min.css";
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+messaging.usePublicVapidKey(process.env.VUE_APP_PUBLIC_VAPID_KEY);
+
+messaging
+  .requestPermission()
+  .then(() => {
+    console.log("Notification permission granted.");
+
+    // Get Token
+    messaging.getToken().then(token => {
+      console.log(token);
+    });
+  })
+  .catch(err => {
+    console.log("Unable to get permission to notify.", err);
+  });
 
 Vue.use(Vuetify, { iconfont: "md" });
 
