e894b97b03a5711ffc748e042a70cad7c89d4a07
RJ <rakhimov.5556@gmail.com>
2019-07-25 18:27:04 +0500

    feat: vueOffline mixin & Default layout

---
+++ web-ts/src/layouts/Default.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-app>
+    <v-container class="pb-5" fluid>
+      <v-layout>
+        <v-toolbar :color="color" dark>
+          <v-toolbar-title>IUTimeTable</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn flat icon color="white">
+            <v-icon>help</v-icon>
+          </v-btn>
+        </v-toolbar>
+      </v-layout>
+
+      <v-layout>
+        <transition name="fade" mode="out-in">
+          <router-view />
+        </transition>
+      </v-layout>
+
+      <v-layout>
+        <v-bottom-nav :active="bottomNav" :color="color" :value="true" :app="true" fixed shift dark>
+          <v-btn value="timetable" @click="changeRoute('timetable')" dark>
+            <span>Timetable</span>
+            <v-icon>chrome_reader_mode</v-icon>
+          </v-btn>
+
+          <v-btn value="groupmates" @click="changeRoute('groupmates')" dark>
+            <span>Groupmates</span>
+            <v-icon>people_outline</v-icon>
+          </v-btn>
+
+          <v-btn value="friends" @click="changeRoute('friends')" dark>
+            <span>Friends</span>
+            <v-icon>group</v-icon>
+          </v-btn>
+
+          <v-btn value="profile" @click="changeRoute('profile')" dark>
+            <span>Profile</span>
+            <v-icon>face</v-icon>
+          </v-btn>
+        </v-bottom-nav>
+      </v-layout>
+
+      <v-snackbar v-model="snackbar" :top="true" :timeout="4000">
+        {{ snackbarText }}
+        <v-btn :color="color" flat @click="snackbar = false">Close</v-btn>
+      </v-snackbar>
+    </v-container>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Mixins, Vue } from "vue-property-decorator";
+
+import VueOfflineMixin from "../mixins/vueOffline";
+import UtilsMixin from "../mixins/utils";
+
+@Component
+export default class Default extends Mixins(UtilsMixin, VueOfflineMixin) {
+  private snackbar: boolean = false;
+  private snackbarText: string = "";
+
+  get bottomNav(): string {
+    return this.$route.name === "friends-timetable"
+      ? "friends"
+      : this.$route.name;
+  }
+
+  changeRoute(to) {
+    this.$router.replace({ name: to });
+  }
+
+  mounted() {
+    this.$on("offline", () => {
+      this.snackbarText = "You are offline ☹️";
+      this.snackbar = true;
+    });
+
+    this.$on("online", () => {
+      this.snackbarText = "You are online 😎️";
+      this.snackbar = true;
+    });
+
+    if (this.isOffline) {
+      this.snackbarText = "You are offline ☹️";
+      this.snackbar = true;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+// TODO: Check style \./
+.vue-pull-to-wrapper > .action-block > .default-text {
+  color: rgba(0, 0, 0, 0.54);
+  margin-bottom: 0;
+  font-weight: 500;
+}
+
+.container.fluid {
+  padding: 0;
+}
+
+.v-toolbar {
+  z-index: 1;
+}
+</style>
---
+++ web-ts/src/mixins/vueOffline.ts
@@ -0,0 +1,34 @@
+import Vue from "vue";
+import { Component, Emit } from "vue-property-decorator";
+
+@Component
+export default class VueOffline extends Vue {
+  isOnline: boolean = false;
+  isOffline: boolean = false;
+
+  @Emit("online")
+  onlineHandler() {
+    this.isOnline = true;
+    this.isOffline = false;
+  }
+
+  @Emit("offline")
+  offlineHandler() {
+    this.isOnline = false;
+    this.isOffline = true;
+  }
+
+  mounted() {
+    if (typeof window !== "undefined") {
+      navigator.onLine ? (this.isOnline = true) : (this.isOffline = true);
+
+      window.addEventListener("online", this.onlineHandler);
+      window.addEventListener("offline", this.offlineHandler);
+
+      this.$once("hook:beforeDestroy", () => {
+        window.removeEventListener("online", this.onlineHandler);
+        window.removeEventListener("offline", this.offlineHandler);
+      });
+    }
+  }
+}
